be2c07af411684d4b555eb90e4645eac00635c0e
Kyla <kylag44@gmail.com>
2018-06-25 16:57:31 -0400

    made sports array

---
+++ Sports/Sports/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  Sports
+//
+//  Created by Kyla  on 2018-06-25.
+//  Copyright © 2018 Kyla . All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
